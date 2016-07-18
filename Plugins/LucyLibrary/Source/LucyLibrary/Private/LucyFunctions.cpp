988dfc83e4d19d4947040ce95f11ed40f67b2ffb
Krzysiek Justyński <krzysiek.justynski@gmail.com>
2016-07-19 00:50:52 +0200

    + bare Lucy Library plugin

---
+++ Plugins/LucyLibrary/Source/LucyLibrary/Private/LucyFunctions.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "LucyLibraryPrivatePCH.h"
+#include "LucyFunctions.h"
+
+ULucyFunctions::ULucyFunctions(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float ULucyFunctions::LucyLibrarySampleFunction(float Param)
+{
+	return -1;
+}
+
---
+++ Source/Lucy/Lucy.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lucy.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lucy, "Lucy" );
